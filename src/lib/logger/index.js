a2bb7a16a4b6295761f3866b93c7cff483574b22
Brooks Becton <broabect@ut.utm.edu>
2018-01-30 10:20:42 -0600

    Added timestamp to testing logs

--- src/lib/logger/index.js
+++ src/lib/logger/index.js
@@ -27,6 +27,7 @@ const logger = winston.createLogger({
 if (process.env.NODE_ENV !== 'production') {
   logger.add(new winston.transports.Console({
     format: winston.format.simple(),
+    timestamp: true,
   }));
 }
 
