cf39a3eec4463c80fa7ee4287472ee0e4caf1d19
Brooks Becton <broabect@ut.utm.edu>
2018-01-29 19:34:22 -0600

    Added flow to eslint

--- src/lib/getRandomInt/index.js
+++ src/lib/getRandomInt/index.js
@@ -1,8 +1,10 @@
+// @flow
+
 /**
  * Takes a max integer and returns a random number below that number
  * @param {Number} max
  */
-function getRandomInt(max) {
+function getRandomInt(max: number): number {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
