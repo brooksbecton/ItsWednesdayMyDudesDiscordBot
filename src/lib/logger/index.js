fcaf0aedd052d207aa0ee35f2e1bfacaaa9d5fff
Brooks Becton <broabect@ut.utm.edu>
2018-01-30 20:12:36 -0600

    Trying a different logger

--- src/lib/logger/index.js
+++ src/lib/logger/index.js
@@ -1,34 +1,10 @@
-const winston = require('winston');
+const { createLogger, format, transports } = require('winston');
 
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  transports: [
-    //
-    // - Write to all logs with level `info` and below to `combined.log`
-    // - Write all logs error (and below) to `error.log`.
-    //
-    new winston.transports.Console({
-      filename: 'error.log',
-      level: 'error',
-      timestamp: true,
-    }),
-    new winston.transports.Console({
-      filename: 'combined.log',
-      timestamp: true,
-    }),
-  ],
+const { combine, timestamp } = format;
+
+const logger = createLogger({
+  format: combine(timestamp()),
+  transports: [new transports.Console(), new transports.File({ filename: 'logs/combined.log' })],
 });
-
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple(),
-    timestamp: true,
-  }));
-}
 
 module.exports = logger;
